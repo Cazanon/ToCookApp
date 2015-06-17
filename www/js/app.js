8fe984b45c8db1e412243d42869ee826485e64f6
cazanon <carlos.azanon@gmail.com>
2015-06-17 21:14:58 +0200

    Update config

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,13 @@
+
+// Modificar listado de recetas
+// Boton de listado y cuadricula
+// Add mongoDB
+// Rendimiento
+// Imagenes desde el servidor
+// CSS
+// Lista Favoritos
+ 
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
