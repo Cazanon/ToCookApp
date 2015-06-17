827cf8c2af757c24f4d564b1e8ae49796435f938
Cazanon <carlos.azanon@gmail.com>
2015-06-17 12:21:39 +0200

    Recipes by category fix (Thanks to Anthanh)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,88 +2,56 @@
 angular.module('controllers', ['services'])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+	//With the new view caching in Ionic, Controllers are only called
+	//when they are recreated or on app start, instead of every page change.
+	//To listen for when this page is active (for example, to refresh data),
+	//listen for the $ionicView.enter event:
+	//$scope.$on('$ionicView.enter', function(e) {});
   
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  
-  // Form data for the login modal
-  $scope.loginData = {};
+	//Form data for the login modal
+	$scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+	//Create the login modal that we will use later
+	$ionicModal.fromTemplateUrl('templates/login.html', {
+		scope: $scope
+	}).then(function(modal) {
+		$scope.modal = modal;
+	});
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+	//Triggered in the login modal to close it
+	$scope.closeLogin = function() {
+		$scope.modal.hide();
+	};
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+	//Open the login modal
+	$scope.login = function() {
+		$scope.modal.show();
+	};
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+	//Perform the login action when the user submits the login form
+	$scope.doLogin = function() {
+		console.log('Doing login', $scope.loginData);
+		//Simulate a login delay. Remove this and replace with your login
+		//code if using a login system
+		$timeout(function() {
+			$scope.closeLogin();
+		}, 1000);
+	};
 })
 
 .controller('RecipesCtrl', function($scope, Recipe) {
-    $scope.recipes = Recipe.query();
+	$scope.recipes = Recipe.query();
 })
 
 .controller('RecipeCtrl', function($scope, $stateParams, Recipe) {
     $scope.recipe = Recipe.get({recipeId: $stateParams.recipeId});
 })
 
-
-.controller('RecipesCategoryCtrl', function($scope, $stateParams, Recipe) {
-    $scope.recipes =  Recipe.get({category: $stateParams.category});
+.controller('RecipesCategoryCtrl', function($scope, $stateParams, RecipesCategory) {
+	$scope.recipes = RecipesCategory.query({
+		category: $stateParams.category
+	});
 })
-
-/*
-.controller('RecipesCategoryCtrl', function($scope, $stateParams, Recipe) {
-	Recipe.get({category: $stateParams.category}).$promise.then(function(response){$scope.recipes = response.data;});
-})*/
-
-
-
-
-/*
-el servicio de ionic
-Recipe.get({category: $stateParams.category});
-}).then(function(response) {});
-y dentro de la función  $scope.recipes = response.data;*/
-
-
-/*.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});*/
-
 
 .controller('PlaylistsCtrl', function($scope) {
 	$scope.steps = [];
--- www/js/services.js
+++ www/js/services.js
@@ -2,4 +2,16 @@ angular.module('services', ['ngResource'])
 
 .factory('Recipe', function ($resource) {
     return $resource('https://tocook-nodeserver.herokuapp.com/recipes/:recipeId');
+})
+
+.factory('RecipesCategory', function($resource) {	
+	return $resource('https://tocook-nodeserver.herokuapp.com/recipes/category/:category', {
+		category: '@category'
+		},{
+			'query': {
+				method: 'GET',
+				isArray: true
+			}
+		}
+	);
 });
